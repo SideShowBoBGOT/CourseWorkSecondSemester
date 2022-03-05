9067d4ed5fa959ed0d5150d3a8cd892b14e4995a
sideshowbobgot <sideshowbobgot@gmail.com>
2022-03-05 12:55:10 +0200

    ships are drawn

--- game_objects.py
+++ game_objects.py
@@ -1,55 +1,56 @@
 import random
+from constants import MAP_SIZE, SHIPS_SIZES
 
 
 class Battleship:
     def __init__(self, size):
-        self.row = random.randint(0, 10)
-        self.col = random.randint(0, 10)
+        self.row = random.randint(0, MAP_SIZE - 1)
+        self.col = random.randint(0, MAP_SIZE - 1)
         self.size = size
         self.orientation = random.choice(["h", "v"])
         self.indexes = self.compute_indexes()
         self.ship_indexes = self.indexes[:size]
 
     def compute_indexes(self):
-        start_index = self.row * 11 + self.col
+        start_index = self.row * MAP_SIZE + self.col
         result_indexes = []
         if self.orientation == 'h':
             indexes = [start_index + i for i in range(self.size)]
-            top_left_padding = [indexes[0] - 1 - 11]
+            top_left_padding = [indexes[0] - 1 - MAP_SIZE]
             left_padding = [indexes[0] - 1]
-            bottom_left_padding = [indexes[0] - 1 + 11]
-            bottom_padding = [i + 11 for i in indexes]
-            bottom_right_padding = [indexes[-1] + 1 + 11]
+            bottom_left_padding = [indexes[0] - 1 + MAP_SIZE]
+            bottom_padding = [i + MAP_SIZE for i in indexes]
+            bottom_right_padding = [indexes[-1] + 1 + MAP_SIZE]
             right_padding = [indexes[-1] + 1]
-            top_right_padding = [indexes[-1] + 1 - 11]
-            top_padding = [i - 11 for i in indexes]
+            top_right_padding = [indexes[-1] + 1 - MAP_SIZE]
+            top_padding = [i - MAP_SIZE for i in indexes]
             result_indexes += indexes
         elif self.orientation == 'v':
-            indexes = [start_index + i * 11 for i in range(self.size)]
-            top_left_padding = [indexes[0] - 1 - 11]
+            indexes = [start_index + i * MAP_SIZE for i in range(self.size)]
+            top_left_padding = [indexes[0] - 1 - MAP_SIZE]
             left_padding = [i - 1 for i in indexes]
-            bottom_left_padding = [indexes[-1] - 1 + 11]
-            bottom_padding = [indexes[-1] + 11]
-            bottom_right_padding = [indexes[-1] + 1 + 11]
+            bottom_left_padding = [indexes[-1] - 1 + MAP_SIZE]
+            bottom_padding = [indexes[-1] + MAP_SIZE]
+            bottom_right_padding = [indexes[-1] + 1 + MAP_SIZE]
             right_padding = [i + 1 for i in indexes]
-            top_right_padding = [indexes[0] + 1 - 11]
-            top_padding = [indexes[0] - 11]
+            top_right_padding = [indexes[0] + 1 - MAP_SIZE]
+            top_padding = [indexes[0] - MAP_SIZE]
             result_indexes += indexes
-        if left_padding[0] // 11 == indexes[0] // 11:
+        if left_padding[0] // MAP_SIZE == indexes[0] // MAP_SIZE:
             result_indexes += left_padding
         if top_padding[0] >= 0:
             result_indexes += top_padding
-        if left_padding[0] // 11 == indexes[0] // 11 and top_padding[0] >= 0:
+        if left_padding[0] // MAP_SIZE == indexes[0] // MAP_SIZE and top_padding[0] >= 0:
             result_indexes += top_left_padding
-        if bottom_padding[0] <= 121:
+        if bottom_padding[0] <= MAP_SIZE ** 2:
             result_indexes += bottom_padding
-        if left_padding[0] // 11 == indexes[0] // 11 and bottom_padding[0] <= 121:
+        if left_padding[0] // MAP_SIZE == indexes[0] // MAP_SIZE and bottom_padding[0] <= MAP_SIZE ** 2:
             result_indexes += bottom_left_padding
-        if right_padding[0] // 11 == indexes[0] // 11:
+        if right_padding[0] // MAP_SIZE == indexes[0] // MAP_SIZE:
             result_indexes += right_padding
-        if right_padding[0] // 11 == indexes[0] // 11 and bottom_padding[0] <= 121:
+        if right_padding[0] // MAP_SIZE == indexes[0] // MAP_SIZE and bottom_padding[0] <= MAP_SIZE ** 2:
             result_indexes += bottom_right_padding
-        if right_padding[0] // 11 == indexes[0] // 11 and top_padding[0] >= 0:
+        if right_padding[0] // MAP_SIZE == indexes[0] // MAP_SIZE and top_padding[0] >= 0:
             result_indexes += top_right_padding
         return result_indexes
 
@@ -57,8 +58,12 @@ class Battleship:
 class Player:
     def __init__(self):
         self.ships = []
-        self.search = ['U' for i in range(121)]  # 'U' for unknown
-        self.place_ships(sizes=[5, 4])
+        self.search = ['U' for _ in range(MAP_SIZE ** 2)]  # 'U' for unknown
+        self.place_ships(sizes=SHIPS_SIZES)
+        self.indexes = [
+            ind for sub in list(map(lambda x: x.ship_indexes, self.ships))
+            for ind in sub
+        ]
 
     def place_ships(self, sizes):
         for size in sizes:
@@ -69,12 +74,12 @@ class Player:
                 # check if placement is possible
                 possible = True
                 for i in ship.indexes[:size]:
-                    # indexes must be less than 121
-                    if i >= 121:
+                    # indexes must be less than MAP_SIZE**2
+                    if i >= MAP_SIZE ** 2:
                         possible = False
                         break
                     # ships cannot go beyond grid
-                    new_row = i // 11
+                    new_row = i // MAP_SIZE
                     if ship.orientation == 'h' and new_row != ship.row:
                         possible = False
                         break
@@ -90,13 +95,11 @@ class Player:
                     self.ships.append(ship)
                     placed = True
 
+    def show_ships(self):
+        indexes = ['-' if i not in self.indexes else 'X' for i in range(MAP_SIZE ** 2)]
+        for row in range(MAP_SIZE):
+            print(' '.join(indexes[(row - 1) * MAP_SIZE:row * MAP_SIZE]))
 
 
 b = Player()
-for i in range(11):
-    for j in range(11):
-        if i*11 + j in [ind for sub in list(map(lambda x: x.ship_indexes, b.ships)) for ind in sub] :
-            print('#', end='')
-        else:
-            print('.', end='')
-    print('\n', end='')
+b.show_ships()
--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import pygame
-
+from constants import MAP_SIZE
+from game_objects import Player, Battleship
 pygame.init()
 pygame.display.set_caption("Battleship")
 
 # global variables
+
 sq_size = 30
 h_margin = sq_size * 4
 v_margin = sq_size
-width = sq_size * 11 * 2 + h_margin
-height = sq_size * 11 * 2 + v_margin
+width = sq_size * MAP_SIZE * 2 + h_margin
+height = sq_size * MAP_SIZE * 2 + v_margin
 screen = pygame.display.set_mode((width, height))
 
 # colors
 grey = (40, 50, 60)
 white = (255, 250, 250)
-
+green = (50, 200, 150)
 
 # function to draw a grid
 def draw_grid(left=0, top=0):
     for i in range(121):
-        x = left + i % 11 * sq_size
-        y = top + i // 11 * sq_size
+        x = left + i % MAP_SIZE * sq_size
+        y = top + i // MAP_SIZE * sq_size
         square = pygame.Rect(x, y, sq_size, sq_size)
         pygame.draw.rect(screen, white, square, width=3)
 
-
+# function to draw ships onto the position grids
+def draw_ships(player, left=0, top=0):
+    for ship in player.ships:
+        x = left + ship.col * sq_size
+        y = top + ship.row * sq_size
+        if ship.orientation == 'h':
+            width = ship.size * sq_size
+            height = sq_size
+        elif ship.orientation == 'v':
+            width = sq_size
+            height = ship.size * sq_size
+        rectangle = pygame.Rect(x, y, width, height)
+        pygame.draw.rect(screen, green, rectangle)
 
 # pygame loop
+player = Player()
 run = True
 pause = False
 while run:
@@ -40,10 +55,18 @@ while run:
             if event.key == pygame.K_SPACE:
                 pause = not pause
     if not pause:
+        # draw background
         screen.fill(grey)
-        draw_grid()
-        draw_grid(left=(width-h_margin)//2 + h_margin)
 
-        draw_grid(left=(width-h_margin)//2 + h_margin, top=(height-v_margin)//2 + v_margin)
-        draw_grid(top=(height-v_margin)//2 + v_margin)
+        # draw search grids
+        draw_grid()
+        draw_grid(left=(width - h_margin) // 2 + h_margin)
+
+        # draw position grids
+        draw_grid(left=(width - h_margin) // 2 + h_margin, top=(height - v_margin) // 2 + v_margin)
+        draw_grid(top=(height - v_margin) // 2 + v_margin)
+
+        # draw ships onto position grids
+        draw_ships(player)
+
         pygame.display.flip()
